418041e62948504c24ca4451093c6e1a63f46055
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-03-03 15:32:24 +0100

    Create Attempt migration

---
+++ WordleS/WordleS/Data/Migrations/20220303143039_CreateAttemptSchema.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WordleS.Data.Migrations
+{
+    public partial class CreateAttemptSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Attempt",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<bool>(type: "bit", nullable: false),
+                    Position = table.Column<bool>(type: "bit", nullable: false),
+                    GameId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attempt", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attempt_Game_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Game",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attempt_GameId",
+                table: "Attempt",
+                column: "GameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Attempt");
+        }
+    }
+}
