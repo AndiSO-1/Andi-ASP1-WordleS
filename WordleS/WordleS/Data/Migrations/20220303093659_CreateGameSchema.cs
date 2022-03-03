a97a3491f8f493a656f84ff79012ff23fc1946d7
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-03-03 15:24:36 +0100

    Create Game migration

---
+++ WordleS/WordleS/Data/Migrations/20220303093659_CreateGameSchema.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WordleS.Data.Migrations
+{
+    public partial class CreateGameSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Game",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Finished = table.Column<bool>(type: "bit", nullable: false),
+                    Win = table.Column<bool>(type: "bit", nullable: false),
+                    MaxAttempt = table.Column<int>(type: "int", nullable: false),
+                    WordId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Game", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Game_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Game_Word_WordId",
+                        column: x => x.WordId,
+                        principalTable: "Word",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_UserId",
+                table: "Game",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Game_WordId",
+                table: "Game",
+                column: "WordId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Game");
+        }
+    }
+}
