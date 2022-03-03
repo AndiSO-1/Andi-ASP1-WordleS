40bb51c8b22867db14764f0c8f6e66854b288576
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-03-03 08:47:12 +0100

    Create Word migration

---
+++ WordleS/WordleS/Data/Migrations/20220303073949_CreateWordsSchema.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WordleS.Data.Migrations
+{
+    public partial class CreateWordsSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Word",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Value = table.Column<string>(type: "nvarchar(5)", maxLength: 5, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Word", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Word");
+        }
+    }
+}
