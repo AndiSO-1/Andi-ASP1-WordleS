6843fb073aa8358f9187434c5bb0c5b426a9919c
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-03-03 15:24:13 +0100

    Create Model Game

---
+++ WordleS/WordleS/Models/Game.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WordleS.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public bool Finished { get; set; }
+
+        [Required]
+        public bool Win { get; set; }
+
+        [Required]
+        public int MaxAttempt { get; set; }
+
+        [Required]
+        public Word Word { get; set; }
+
+        [Required]
+        public IdentityUser User { get; set; }
+    }
+}
