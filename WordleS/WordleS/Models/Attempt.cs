db4d2280545a4c012e73d072b7f262103cdd8d56
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-03-03 15:31:09 +0100

    Create Attempt Model

---
+++ WordleS/WordleS/Models/Attempt.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WordleS.Models
+{
+    public class Attempt
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public bool Value { get; set; }
+
+        [Required]
+        public bool Position { get; set; }
+
+        [Required]
+        public Game Game { get; set; }
+    }
+}
