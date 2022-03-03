60650fac530916b61d8cf4a67e251eb049427cc8
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-03-03 08:43:14 +0100

    Create Model Word

---
+++ WordleS/WordleS/Models/Word.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WordleS.Models
+{
+    public class Word
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(5), MinLength(5)]
+        public string Value { get; set; }
+    }
+}
